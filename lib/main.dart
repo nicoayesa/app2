9a605cd72a0e8beb730a6efe13effcab2250d3cf
nicoayesa <nico.ayesa@gmail.com>
2023-11-06 17:07:20 +0100

    App2 final push

--- lib/main.dart
+++ lib/main.dart
@@ -9,26 +9,41 @@ void main() {
         ),
         body:
         Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
-            Container(
-              decoration: BoxDecoration(
-                color: Colors.red,
-              ),
-              child: Text('Nicolas Ayesa', style: TextStyle(fontSize: 20), textAlign: TextAlign.center,),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: <Widget>[
+                Container(
+                  decoration: BoxDecoration(
+                    color: Colors.red,
+                  ),
+                  child: Text('Nicolas Ayesa', style: TextStyle(fontSize: 20), textAlign: TextAlign.center,),
+                ),
+                Container(
+                  decoration: BoxDecoration(
+                    color: Colors.green,
+                  ),
+                  child: Text('S2AM', style: TextStyle(fontSize: 20), textAlign: TextAlign.center,),
+                ),
+                Container(
+                  decoration: BoxDecoration(
+                    color: Colors.blue,
+                  ),
+                  child: Text('23-24', style: TextStyle(fontSize: 20), textAlign: TextAlign.center,),
+                ),
+              ],
             ),
-            Container(
-              decoration: BoxDecoration(
-                color: Colors.green,
-              ),
-              child: Text('S2AM', style: TextStyle(fontSize: 20), textAlign: TextAlign.center,),
-            ),
-            Container(
-              decoration: BoxDecoration(
-                color: Colors.blue,
-              ),
-              child: Text('23-24', style: TextStyle(fontSize: 20), textAlign: TextAlign.center,),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Text(
+                    'Nicolás Ayesa, S2AM 23-24', 
+                    style: TextStyle(fontSize: 20),
+                    textAlign: TextAlign.center,
+                    ),
+              ],
             )
           ],
         )
